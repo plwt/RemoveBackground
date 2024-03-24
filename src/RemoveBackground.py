dd3679242e86c28a143276f0dd84b92a4a808346
Paul Wright <31939113+plwt@users.noreply.github.com>
2024-03-24 22:25:39 +0000

    Text correction

--- src/RemoveBackground.py
+++ src/RemoveBackground.py
@@ -18,24 +18,28 @@ welcome()
 # Remove Backgound
 def removebackground():
     # Ask the user to confirm the image has been saved
-    Gonogo=input("Have you saved the file to be watermarked to the RemoveBackground folder in your user documents with filename image.jpg?(y/n)")
+    Gonogo=input("Have you saved the file to be edited to the RemoveBackground folder in your user documents with filename image.jpg?(y/n)")
     
     if Gonogo=="n":
-        print("Please save the image to be watermarked to the RemoveBackground folder in your user documents with the filename image.jpg and run RemoveBackground again.")
+        print("Please save the image to be edited to the RemoveBackground folder in your user documents with the filename image.jpg and run RemoveBackground again.")
           
 
     elif Gonogo=="y":
         
         from pathlib import Path
         home_path = str(Path.home())
+        
+        # Set in-bound file path
         input_path = (home_path + '/RemoveBackground/image.jpg')
+        
+        # Set out-bound file path
         output_path = (home_path + '/RemoveBackground/output.png')
 
         input = Image.open(input_path)
         output = remove(input)
         output.save(output_path)
 
-        # Confirm the watermarked image has been saved
+        # Confirm the edited image has been saved
         print("Your edited file has been saved to the RemoveBackground folder in your user documents with the filename output.png.  Thank you for using RemoveBackground")
     else:
         print("Please try again.")
