8dd33f35fc6eb2a70cd28dd4de88e12fb3bef4b2
Paul Wright <31939113+plwt@users.noreply.github.com>
2024-05-22 22:09:38 +0100

    Refactor

--- src/RemoveBackground.py
+++ src/RemoveBackground.py
@@ -1,47 +1,17 @@
-# install required packages
-from PIL import Image
-from rembg import remove
+python
 import os
-import sys
-from pathlib import Path
+from rembg import remove
 
+def remove_background(image_path):
+    output_path = os.path.join(os.path.dirname(image_path), "output.png")
+    with open(image_path, 'rb') as i:
+        with open(output_path, 'wb') as o:
+            o.write(remove(i.read()))
 
-# Welcome message
-def welcome():
-    print("""
-    Welcome to RemoveBackground
-    """)
+def main():
+    image_path = "path_to_your_image.jpg"  # Replace with the path to your image file
+    remove_background(image_path)
+    print("Background removed successfully. The output image is saved as output.png in the same directory as the input image.")
 
-welcome()
-
-
-# Remove Backgound
-def removebackground():
-    # Ask the user to confirm the image has been saved
-    Gonogo=input("Have you saved the file to be edited to the RemoveBackground folder in your user documents with filename image.jpg?(y/n)")
-    
-    if Gonogo=="n":
-        print("Please save the image to be edited to the RemoveBackground folder in your user documents with the filename image.jpg and run RemoveBackground again.")
-          
-
-    elif Gonogo=="y":
-        
-        from pathlib import Path
-        home_path = str(Path.home())
-        
-        # Set in-bound file path
-        input_path = (home_path + '/RemoveBackground/image.jpg')
-        
-        # Set out-bound file path
-        output_path = (home_path + '/RemoveBackground/output.png')
-
-        input = Image.open(input_path)
-        output = remove(input)
-        output.save(output_path)
-
-        # Confirm the edited image has been saved
-        print("Your edited file has been saved to the RemoveBackground folder in your user documents with the filename output.png.  Thank you for using RemoveBackground")
-    else:
-        print("Please try again.")
-
-removebackground()
+if __name__ == "__main__":
+    main()
--- src/RemoveBackground.sh
+++ src/RemoveBackground.sh
@@ -6,9 +6,6 @@ python3 -m venv venv
 source venv/bin/activate
 
 # install requirements
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade Pillow
-python3 -m pip install --upgrade pathlib
 python3 -m pip install --upgrade rembg
 
 # make folders
