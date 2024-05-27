6553c88baf0485c384c814e8485ab318201263ac
Paul Wright <31939113+plwt@users.noreply.github.com>
2024-05-27 21:13:01 +0100

    Added code to add image on top of a white background (1st attempt)

--- src/RemoveBackground.py
+++ src/RemoveBackground.py
@@ -1,11 +1,20 @@
 import os
 from rembg import remove
+from PIL import Image
+import os
+
 
 def remove_background(image_path):
     output_path = os.path.join(os.path.dirname(image_path), "output.png")
     with open(image_path, 'rb') as i:
         with open(output_path, 'wb') as o:
             o.write(remove(i.read()))
+    output=Image.open("/opt/RemoveBackground/output.png")
+    hmat,wmat = output.size
+    background=Image.open("opt/RemoveBackground/background.png")
+    resizebackground=background.resize((hmat,wmat))
+    resizebackground.paste(output,box=(0,0)background=output)
+    resizebackground.save(image_path + "complete.png")
 
 def main():
     image_path = "/opt/RemoveBackground/image.jpg"
