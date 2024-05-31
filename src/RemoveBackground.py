6a99eeb2907634fe9a5c9be53d44780f24401741
Paul Wright <31939113+plwt@users.noreply.github.com>
2024-05-31 23:01:13 +0100

    Update RemoveBackground.py

--- src/RemoveBackground.py
+++ src/RemoveBackground.py
@@ -6,10 +6,10 @@ import os
 
 def remove_background(image_path):
     output_path = os.path.join(os.path.dirname(image_path), "output.png")
-    with open(image_path, 'rb') as i:
-        with open(output_path, 'wb') as o:
+    with open(image_path, "rb") as i:
+        with open(output_path, "wb") as o:
             o.write(remove(i.read()))
-    
+
     # Open the images
     output_image = Image.open("output.png")
     background_image = Image.open("background.png")
@@ -28,16 +28,15 @@ def remove_background(image_path):
 
     # Save the new image
     new_image.save("combined_image.png")
-    
-    
-    
-    
+
+
 def main():
     image_path = "/opt/RemoveBackground/image.jpg"
     remove_background(image_path)
-    print("Background removed successfully. The output image is saved as output.png in the same directory as the input image.")
+    print(
+        "Background removed successfully. The output image is saved as output.png in the same directory as the input image."
+    )
+
 
 if __name__ == "__main__":
     main()
-
-
